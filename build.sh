c2dbf267efb3a99bbadcc31323a682cd9da08cb9
Rajarshi Das <rajarshid@cybage.com>
2013-07-17 17:24:45 +0530

    fix some code

--- build.sh
+++ build.sh
@@ -1,18 +1,3 @@
-#! /bin/sh
-# build.sh
-########################################################################
-
-########################################################################
-# BUILD WAR PACKAGE FROM RUBY SCRIPTS
-#
-# This shell script builds a war package that can be installed in a
-# JBoss instance.
-#
-########################################################################
-
-########################################################################
-# RUNNING VARIOUS RUBY INSTALLERS
-#
 echo "Installing Gem's bundler"
 if ! gem install bundler
 then
@@ -25,21 +10,6 @@ if ! bundle install
 then
     echo "Installtion of Gemfile Bundle failed"
     exit 3
-fi
-
-echo "Creating 'build_mbe_prilfe'"
-BUILD_KEY="build_mbe_profile"
-if ! rake build_mbe_profile
-then
-    echo "Installtion of build_mbe_profile failed"
-    exit 4
-fi
-
-echo "Running Rake spec"
-if ! rake ci:setup:rspec spec
-then
-    echo "Rake spec failed"
-    exit 5
 fi
 
 echo "Creating WAR file via Warble"
